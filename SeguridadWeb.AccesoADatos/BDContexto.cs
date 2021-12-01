e8d810056b81ac903bcc41a57bbb6a39c6515979
Stephanie064h <90538053+Stephanie064h@users.noreply.github.com>
2021-11-30 18:04:25 -0600

    Archivos del auto completado

---
+++ SeguridadWeb.AccesoADatos/BDContexto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// ********************************
+using Microsoft.EntityFrameworkCore;
+using SeguridadWeb.EntidadesDeNegocio;
+
+namespace SeguridadWeb.AccesoADatos
+{
+    public class BDContexto : DbContext
+    {
+        public DbSet<Rol> Rol { get; set; }
+        public DbSet<Usuario> Usuario { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=DESKTOP-DEV01\SQLEXPRESS;Initial Catalog=SeguridadWebdb;Integrated Security=True");
+        }
+    }
+}
